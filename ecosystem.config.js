c299ef9dbf2c2b2cf642a91490b5e6fd5d58581d
Hugo Da Roit <contact@hdaroit.fr>
2020-04-09 22:43:10 +0200

    wip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,6 +15,8 @@ module.exports = {
         POSTGRESQL_USER: 'paffme',
         POSTGRESQL_PASSWORD: 'paffme',
         POSTGRESQL_DATABASE: 'paffme',
+        JWT_SECRET:
+          'vFP8CTP9svUPYHD/rBnzdh2kTDxj3ZllnvsYGTo1oz7QL0e2y3NuEejHFO/Gzp5d+HHgHHXD9twTo0Rj5ssQrwRxVieANBLkIxkAhTFPAHJPGXbtJZvfIS2b64SQFkFY7GIWrIhRvc6XHuFlPi+RtmvNmze/8AwwowL4/q32mrQF/xqOszS82DJL94GjB1HNOvw8W+oPL9XIHBzZB00eBjgKnVN0oYcRXL9JQPThyTTx1ArTCVbpnQHNXcCDjgUyxVO7kfU7sTJIwWFJrLmodpgwLBNaYgkSMSzWN+3HGP9QfielN2VmIrKSWAZGVePtx8DLk39cvaqjOAFbXrdROg==',
       },
     },
   ],
