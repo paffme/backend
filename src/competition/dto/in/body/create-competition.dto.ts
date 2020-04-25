29ed8a4d7dd43ad876a4a1bbb144c07efd5fa68d
Hugo Da Roit <contact@hdaroit.fr>
2020-04-25 19:22:31 +0200

    Add missing validations

--- src/competition/dto/in/body/create-competition.dto.ts
+++ src/competition/dto/in/body/create-competition.dto.ts
@@ -3,45 +3,64 @@ import { Sex } from '../../../../shared/types/sex.enum';
 import { CategoryName } from '../../../../shared/types/category-name.enum';
 import { CompetitionType } from '../../../types/competition-type.enum';
 
+import {
+  IsArray,
+  IsDateString,
+  IsEnum,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
 class CategoryDto {
   @ApiProperty({
     enum: Sex,
   })
+  @IsEnum(Sex)
   sex!: Sex;
 
   @ApiProperty({
     enum: CategoryName,
   })
+  @IsEnum(CategoryName)
   name!: CategoryName;
 }
 
 export class CreateCompetitionDTO {
   @ApiProperty()
+  @IsString()
   name!: string;
 
   @ApiProperty({
     enum: CompetitionType,
   })
+  @IsEnum(CompetitionType)
   type!: CompetitionType;
 
   @ApiProperty()
+  @IsDateString()
   startDate!: Date;
 
   @ApiProperty()
+  @IsDateString()
   endDate!: Date;
 
   @ApiProperty()
+  @IsString()
   address!: string;
 
   @ApiProperty()
+  @IsString()
   city!: string;
 
   @ApiProperty()
+  @IsString()
   postalCode!: string;
 
   @ApiProperty({
     type: CategoryDto,
     isArray: true,
   })
+  @IsArray()
+  @ValidateNested({ each: true })
   categories!: CategoryDto[];
 }
--- src/user/dto/in/body/update-user.dto.ts
+++ src/user/dto/in/body/update-user.dto.ts
@@ -1,16 +1,26 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsEmail, IsOptional, IsString, Length } from 'class-validator';
 
 export class UpdateUserDto {
   @ApiPropertyOptional()
   @IsOptional()
+  @IsString()
   readonly username?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
+  @IsEmail()
   readonly email?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    required: true,
+    minLength: 6,
+    maxLength: 32,
+    type: String,
+    format: 'password',
+  })
   @IsOptional()
+  @IsString()
+  @Length(6, 32)
   password?: string;
 }
