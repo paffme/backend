f868fe85e2193e15a44801944c849edfadc8de74
Hugo Da Roit <contact@hdaroit.fr>
2020-06-16 23:52:57 +0200

    increase jwt expiration to 12h

--- config/production.ts
+++ config/production.ts
@@ -7,7 +7,7 @@ const config: Config = {
   PORT: 4000,
   JWT_SECRET: env.JWT_SECRET as string,
   JWT_ALGORITHM: 'HS256',
-  JWT_EXPIRATION: '1h',
+  JWT_EXPIRATION: '12h',
   JWT_ISSUER: 'paffme.hdaroit.fr',
   POSTGRESQL_USER: env.POSTGRESQL_USER as string,
   POSTGRESQL_PASSWORD: env.POSTGRESQL_PASSWORD as string,
