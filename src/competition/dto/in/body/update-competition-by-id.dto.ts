029b6b3f2b1482345561d3080d3ecb33da2f9f87
Hugo Da Roit <contact@hdaroit.fr>
2020-06-17 00:13:10 +0200

    fix: add more validation and tests for it

--- src/competition/dto/in/body/update-competition-by-id.dto.ts
+++ src/competition/dto/in/body/update-competition-by-id.dto.ts
@@ -4,12 +4,15 @@ import { Sex } from '../../../../shared/types/sex.enum';
 import { CategoryName } from '../../../../shared/types/category-name.enum';
 
 import {
+  ArrayNotEmpty,
   IsDateString,
   IsEnum,
   IsOptional,
   IsString,
+  Length,
   ValidateNested,
 } from 'class-validator';
+
 import { Type } from 'class-transformer';
 
 class UpdateCategoryDto {
@@ -30,6 +33,7 @@ export class UpdateCompetitionByIdDto {
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @Length(2, 64)
   name?: string;
 
   @ApiPropertyOptional({
@@ -38,6 +42,18 @@ export class UpdateCompetitionByIdDto {
   @IsOptional()
   @IsEnum(CompetitionType)
   type?: CompetitionType;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @Length(10, 1024)
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @Length(10, 1024)
+  agenda?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
@@ -52,16 +68,19 @@ export class UpdateCompetitionByIdDto {
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @Length(4, 128)
   address?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @Length(1, 64)
   city?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @Length(1, 12)
   postalCode?: string;
 
   @ApiPropertyOptional({
@@ -69,6 +88,7 @@ export class UpdateCompetitionByIdDto {
     isArray: true,
   })
   @IsOptional()
+  @ArrayNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => UpdateCategoryDto)
   categories?: UpdateCategoryDto[];
